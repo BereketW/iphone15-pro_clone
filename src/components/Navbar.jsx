58ee94fa6f0174c2d9c1f23509f3eadd7093b47c
BereketW <bereketsodenoo@gmail.com>
2024-11-12 20:50:53 +0300

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import {appleImg} from "../utils/index.js";
+
+export const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <img src={appleImg} alt="logo" />
+            </nav>
+        </header>
+    )
+}
